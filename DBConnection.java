897922b4396440409234bf865076c5e3b3f884d3
Danish9648 <142091492+Danish9648@users.noreply.github.com>
2025-05-02 16:34:40 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/employee_db";
+    private static final String USER = "root"; // change if needed
+    private static final String PASSWORD = "Danish@123"; // add your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
